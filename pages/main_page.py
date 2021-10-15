73f232b96704955988d4265a6ec30d1b752cdf08
Egor Solenok <solenok.egor@gmail.com>
2021-10-15 22:09:49 +0300

    Added the switch to login page in test for possibility to continue check

--- pages/main_page.py
+++ pages/main_page.py
@@ -4,15 +4,15 @@ import pytest
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
+from .login_page import LoginPage
 
 # Инициализируем класс наследник MainPage для главной страницы сайта
 class MainPage(BasePage):
     #определяем метод для открытия окна логина в систему
     def go_to_login_page(self):
-        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        login_link.click()   
-        
-    #Определяем метод проверки ссылки на логин
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()   
+
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login  link is not presented"
         
--- test_main_page.py
+++ test_main_page.py
@@ -8,19 +8,18 @@ import time
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()                     #открываем страницу
-    page.go_to_login_page()         #выполняем метод страницы - переходим на страницу логина
-
+    page = MainPage(browser, link)
+    # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()      #открываем страницу
+    page.go_to_login_page()
+    #выполняем метод страницы - переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url) 
+    #явно инициализируем страницу логина в браузер
+    login_page.should_be_login_page() 
+    #осуществляем проверку страницы с логином
+    
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()    #метод проверки 
-    
-#def test_guest_should_see_login_page(browser):
-#    link = "https://selenium1py.pythonanywhere.com/accounts/login/"
-#    page = LoginPage(browser, link)
-#    page.open()
-#    page.should_be_login_page()    #метод проверки 
-#
