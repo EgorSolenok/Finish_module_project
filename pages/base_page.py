0a54db7a28a6641e838f31e00d72cc9de92d6882
Egor Solenok <solenok.egor@gmail.com>
2021-10-15 12:43:54 +0300

    Added class BasePage and some files

---
+++ pages/base_page.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import pytest
+import time
+from selenium import webdriver
+
+# Опредлеяем класс для базовой страницы веб-сайта
+class BasePage():
+    # Добавляем конструктор, передаем в него экземпляр драйвера и url адресс
+    # Внутри конструктора сохраняем эти данные как аттрибуты нашего класса
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+    # Добавляем метод открывающий страницу, использующий метод get()
+    def open(self):
+        self.browser.get(self.url)
